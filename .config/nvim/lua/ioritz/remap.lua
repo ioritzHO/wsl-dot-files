8332b6c9e3ad2186d401d81deae580ade692c3c2
Ioritz HO <ioritz.herreros@gmail.com>
2023-11-09 15:10:32 +0100

    Fix DiffToggle function

--- .config/nvim/lua/ioritz/remap.lua
+++ .config/nvim/lua/ioritz/remap.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n","<leader>c","bi//<ESC>")             -- comment from word
 -- vim.keymap.set("n","<leader><space>",":/\s\+$<CR>")     -- look for trailing spaces
 vim.keymap.set("n","<leader>l","<C-w>w")
 vim.keymap.set("n","<leader>s",":set scb!<CR>")         -- toggle scrollbind
-vim.keymap.set("n","<leader>d",":call DiffToggle()<CR>")
+vim.keymap.set("n","<leader>d",":lua DiffToggle()<CR>")
 vim.keymap.set("n","<leader>n",":NERDTreeToggle<CR>")
 vim.keymap.set("n","<leader>u",":UndotreeToggle<CR>")
 vim.keymap.set("n","<leader>f",":Telescope find_files<CR>")
@@ -16,7 +16,7 @@ function DiffToggle()
     -- Test the setting 'diff', to see if it's on or off.
     -- (Any :set option can be tested with &name.
     -- See :help expr-option.)
-    if (vim.cmd("diff")) then
+    if (vim.wo.diff) then
         vim.cmd("diffoff")
     else
         vim.cmd("diffthis")
